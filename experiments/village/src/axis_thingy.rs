cd6dd369daaf75591cbdd2790e0c33c69a3fb7a6
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2014-11-22 16:15:26 +1100

    Attempt to render axis
    no dice though :(

--- experiments/village/src/axis_thingy.rs
+++ experiments/village/src/axis_thingy.rs
@@ -13,9 +13,3 @@ pub const VERTEX_DATA: &'static [Vertex] = &[
     Vertex { pos: [ 0.0,  0.0, -1.0], color: [0.5, 0.5, 1.0] },
     Vertex { pos: [ 0.0,  0.0,  1.0], color: [0.5, 0.5, 1.0] },
 ];
-
-pub const INDEX_DATA: &'static [u8] = &[
-    0, 1,
-    2, 3,
-    4, 5,
-];
