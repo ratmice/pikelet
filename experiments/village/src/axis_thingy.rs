24164465bd8385e92523c82054458d394fa66ed3
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2014-11-24 10:43:58 +1100

    Axis doc comment

--- experiments/village/src/axis_thingy.rs
+++ experiments/village/src/axis_thingy.rs
@@ -2,6 +2,22 @@
 
 use Vertex;
 
+/// Points for an axis widget thingy
+///
+/// ~~~nottrust
+///          [0, 1, 0]
+///              |[0, 0, 1]
+///              |  /
+///              | /
+/// [-1, 0, 0]   |/    [1, 0, 0]
+///      --------o----------
+///             /|
+///            / |
+///           /  |
+///          /   |
+///   [0, 0, -1] |
+///          [0, -1, 0]
+/// ~~~~
 pub const VERTEX_DATA: &'static [Vertex] = &[
     // X axis
     Vertex { pos: [-1.0,  0.0,  0.0], color: [1.0, 0.5, 0.5] },
