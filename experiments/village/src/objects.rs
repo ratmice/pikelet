a5cfe107b92594315e4639acf160fb67833581dc
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2014-12-03 10:10:11 +1100

    Fix tree model orientation

--- experiments/village/src/objects.rs
+++ experiments/village/src/objects.rs
@@ -110,8 +110,8 @@ pub mod tree {
         pub const COLOR: [f32, ..3] = [0.2, 0.2, 0.2];
 
         pub const VERTEX_DATA: &'static [Vertex] = &[
-            Vertex { pos: [ 0.0,  0.01,  0.0], color: COLOR },
-            Vertex { pos: [ 0.0,  0.01,  0.7], color: COLOR },
+            Vertex { pos: [ 0.0,  0.0,  0.01], color: COLOR },
+            Vertex { pos: [ 0.0,  0.7,  0.01], color: COLOR },
         ];
     }
 
@@ -121,9 +121,9 @@ pub mod tree {
         pub const COLOR: [f32, ..3] = [0.2, 0.4, 0.1];
 
         pub const VERTEX_DATA: &'static [Vertex] = &[
-            Vertex { pos: [-0.2,  0.0,  0.3], color: COLOR },
-            Vertex { pos: [ 0.0,  0.0,  1.0], color: COLOR },
-            Vertex { pos: [ 0.2,  0.0,  0.3], color: COLOR },
+            Vertex { pos: [-0.2,  0.3,  0.0], color: COLOR },
+            Vertex { pos: [ 0.2,  0.3,  0.0], color: COLOR },
+            Vertex { pos: [ 0.0,  1.0,  0.0], color: COLOR },
         ];
     }
 }
