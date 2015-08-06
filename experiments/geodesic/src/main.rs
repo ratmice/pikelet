a9f1abb7b53d54e0081d70e5a464930fca739af3
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2015-08-06 19:21:16 +1000

    TRIANGLE

--- experiments/geodesic/src/main.rs
+++ experiments/geodesic/src/main.rs
@@ -1,26 +1,78 @@
+#[macro_use]
+extern crate gfx;
+extern crate gfx_window_glutin;
+extern crate glutin;
+
 use glutin::Event;
 use glutin::ElementState as State;
 use glutin::VirtualKeyCode as KeyCode;
+use glutin::{GlProfile, GlRequest, WindowBuilder};
+use gfx::traits::*;
 
-extern crate glutin;
+gfx_vertex!(Vertex {
+    a_Pos @ pos: [f32; 2],
+    a_Color @ color: [f32; 3],
+});
+
+gfx_parameters!( Params {
+    foo @ foo: i32,
+});
 
 fn main() {
-    let window = glutin::WindowBuilder::new()
+    let window = WindowBuilder::new()
         .with_title("Geodesic Experiment".to_string())
         .with_dimensions(800, 500)
-        .with_gl(glutin::GlRequest::Latest)
-        .with_gl_profile(glutin::GlProfile::Core)
+        .with_gl(GlRequest::Latest)
+        .with_gl_profile(GlProfile::Core)
         .build().unwrap();
 
-    unsafe { window.make_current().unwrap() };
+    let (mut stream, mut device, mut factory) = gfx_window_glutin::init(window);
 
-    for event in window.wait_events() {
-        window.swap_buffers().unwrap();
+    let vertex_data = [
+        Vertex { pos: [ -0.5, -0.5 ], color: [1.0, 0.0, 0.0] },
+        Vertex { pos: [  0.5, -0.5 ], color: [0.0, 1.0, 0.0] },
+        Vertex { pos: [  0.0,  0.5 ], color: [0.0, 0.0, 1.0] },
+    ];
+    let mesh = factory.create_mesh(&vertex_data);
 
-        match event {
-            Event::Closed => break,
-            Event::KeyboardInput(State::Pressed, _, Some(KeyCode::Escape)) => break,
-            _ => {},
+    let index_data: &[u8] = &[
+        0, 1,
+        1, 2,
+        2, 0,
+    ];
+
+    let vs = gfx::ShaderSource {
+        // glsl_120: Some(include_bytes!("triangle_120.vs")),
+        glsl_150: Some(include_bytes!("triangle_150.vs")),
+        .. gfx::ShaderSource::empty()
+    };
+    let fs = gfx::ShaderSource {
+        // glsl_120: Some(include_bytes!("triangle_120.fs")),
+        glsl_150: Some(include_bytes!("triangle_150.fs")),
+        .. gfx::ShaderSource::empty()
+    };
+    let program = factory.link_program_source(vs, fs).unwrap();
+    let state = gfx::DrawState::new();
+
+    let mut batch = gfx::batch::Full::new(mesh, program, Params { foo: 0, _r: std::marker::PhantomData, }).unwrap();
+    batch.slice = index_data.to_slice(&mut factory, gfx::PrimitiveType::Line);
+    batch.state = batch.state.depth(gfx::state::Comparison::LessEqual, true);
+
+    'main: loop {
+        for event in stream.out.window.poll_events() {
+            match event {
+                Event::Closed => break 'main,
+                Event::KeyboardInput(State::Pressed, _, Some(KeyCode::Escape)) => break 'main,
+                _ => {},
+            }
         }
+
+        stream.clear(gfx::ClearData {
+            color: [0.3, 0.3, 0.3, 1.0],
+            depth: 1.0,
+            stencil: 0,
+        });
+        stream.draw(&batch).unwrap();
+        stream.present(&mut device);
     }
 }
