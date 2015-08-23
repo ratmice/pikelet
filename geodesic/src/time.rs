e2796a88b6881cc7bd6efb2793407ac90347a11d
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2015-08-24 01:06:48 +1000

    Rename Deltas->States

--- geodesic/src/time.rs
+++ geodesic/src/time.rs
@@ -16,12 +16,12 @@ impl<T: Copy + Sub<T, Output = T>> State<T> {
     pub fn delta(&self) -> T { self.current - self.previous }
 }
 
-pub struct Deltas<T: Copy + Sub<T, Output = T>> {
+pub struct States<T: Copy + Sub<T, Output = T>> {
     get_time: fn() -> T,
     previous: T,
 }
 
-impl<T: Copy + Sub<T, Output = T>> Iterator for Deltas<T> {
+impl<T: Copy + Sub<T, Output = T>> Iterator for States<T> {
     type Item = State<T>;
 
     fn next(&mut self) -> Option<State<T>> {
@@ -34,15 +34,15 @@ impl<T: Copy + Sub<T, Output = T>> Iterator for Deltas<T> {
     }
 }
 
-pub fn seconds() -> Deltas<f64> {
-    Deltas {
+pub fn seconds() -> States<f64> {
+    States {
         get_time: lib::precise_time_s,
         previous: lib::precise_time_s(),
     }
 }
 
-pub fn nanoseconds() -> Deltas<u64> {
-    Deltas {
+pub fn nanoseconds() -> States<u64> {
+    States {
         get_time: lib::precise_time_ns,
         previous: lib::precise_time_ns(),
     }
