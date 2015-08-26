ad85759809c7858baec69180c9c7ab752b5e7303
Chip Collier <ccollier@machinezone.com>
2015-08-26 16:50:02 +0200

    adding glium and imgui-rs to the project.

--- voyager/src/main.rs
+++ voyager/src/main.rs
@@ -1,65 +1,91 @@
 
-use resources::ResourceManager;
+extern crate glium;
 
-mod resources;
+use std::thread;
 
-struct Application {
-    target_delta: f64,
-    pub game_time: f64,
-    pub resources: ResourceManager
-}
+use glium::glutin;
 
-impl Application {
-    fn init() -> Application {
-        Application {
-            target_delta: 1.0 / 60.0,
-            game_time: 0.0,
-            resources: ResourceManager
-        }
-    }
+// use resources::ResourceManager;
 
-    fn render(&self) {
-    }
+// mod resources;
 
-    fn update(&self, delta:f64) {
-    }
+// struct Application {
+//     target_delta: f64,
+//     pub game_time: f64,
+//     pub resources: ResourceManager
+// }
 
-    fn run(&mut self) {
-        // TODO
-        //let mut previous_time: f64 = self.platform.get_time();
-        let mut previous_time: f64 = 0.0;
+// impl Application {
+//     fn init() -> Application {
+//         Application {
+//             target_delta: 1.0 / 60.0,
+//             game_time: 0.0,
+//             resources: ResourceManager
+//         }
+//     }
 
-        // TODO
-        //while !self.platform.exit_requested() {
-        let running = false;
-        while running {
-            // TODO
-            // let current_time: f64 = self.platform.get_time();
-            let current_time: f64 = 0.0;
-            let mut frame_time: f64 = current_time - previous_time;
-            previous_time = current_time;
+//     fn render(&self) {
+//     }
 
-            // Gather input and dispatch commands
+//     fn update(&self, delta:f64) {
+//     }
 
-            // Update
-            while frame_time > 0.0 {
-                let delta: f64 = self.target_delta.min(frame_time);
+//     fn run(&mut self) {
+//         // TODO
+//         //let mut previous_time: f64 = self.platform.get_time();
+//         let mut previous_time: f64 = 0.0;
 
-                self.update(delta);
+//         // TODO
+//         //while !self.platform.exit_requested() {
+//         let running = false;
+//         while running {
+//             // TODO
+//             // let current_time: f64 = self.platform.get_time();
+//             let current_time: f64 = 0.0;
+//             let mut frame_time: f64 = current_time - previous_time;
+//             previous_time = current_time;
 
-                frame_time -= delta;
-                self.game_time += delta;
-            }
+//             // Gather input and dispatch commands
 
-            // Draw the world
-            self.render();
-        }
+//             // Update
+//             while frame_time > 0.0 {
+//                 let delta: f64 = self.target_delta.min(frame_time);
 
-        // shutdown
-    }
-}
+//                 self.update(delta);
+
+//                 frame_time -= delta;
+//                 self.game_time += delta;
+//             }
+
+//             // Draw the world
+//             self.render();
+//         }
+
+//         // shutdown
+//     }
+// }
 
 fn main() {
-    let mut app = Application::init();
-    app.run()
+    // let mut app = Application::init();
+    // app.run()
+    use glium::DisplayBuild;
+
+    let display = glium::glutin::WindowBuilder::new()
+        .with_dimensions(1280, 720)
+        .with_title(format!("Hello world"))
+        .build_glium()
+        .unwrap();
+
+    'main: loop {
+        let (w, h) = display.get_framebuffer_dimensions();
+
+        thread::sleep_ms(17);
+
+        for event in display.poll_events() {
+            match event {
+                glutin::Event::Closed => break 'main,
+                _ => ()
+            }
+        }
+    }
 }
