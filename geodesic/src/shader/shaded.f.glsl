c52ed3f2f448a5959b902e12fea2f1c815a68843
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2016-03-08 07:38:19 +1100

    Fix indentation

--- geodesic/src/shader/shaded.f.glsl
+++ geodesic/src/shader/shaded.f.glsl
@@ -10,19 +10,19 @@ out vec4 o_color;
 
 // From https://github.com/stackgl/glsl-face-normal
 vec3 faceNormal(vec3 pos) {
-  vec3 fdx = dFdx(pos);
-  vec3 fdy = dFdy(pos);
-  return normalize(cross(fdx, fdy));
+    vec3 fdx = dFdx(pos);
+    vec3 fdy = dFdy(pos);
+    return normalize(cross(fdx, fdy));
 }
 
 void main() {
-  vec3 ambient = vec3(0.0075, 0.0075, 0.0075); // avoid pure black unless you're out in space.
-  vec3 normal = faceNormal(v_eye_relative_pos);
+    vec3 ambient = vec3(0.0075, 0.0075, 0.0075); // avoid pure black unless you're out in space.
+    vec3 normal = faceNormal(v_eye_relative_pos);
 
-  float intensity = max(dot(light_dir, normal), 0.0);
+    float intensity = max(dot(light_dir, normal), 0.0);
 
-  // TODO: Gather light contributions in a loop (support another light or two).
-  vec3 lighting = min((intensity + ambient), 1.0);
-  
-  o_color = vec4(lighting * color.rgb, 1.0);
+    // TODO: Gather light contributions in a loop (support another light or two).
+    vec3 lighting = min((intensity + ambient), 1.0);
+
+    o_color = vec4(lighting * color.rgb, 1.0);
 }
