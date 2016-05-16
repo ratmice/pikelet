b76d6ebc48232e4cf29d69a7312630999b6a6367
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2016-05-16 21:45:52 +1000

    Pass tick data using events

--- geodesic/src/main.rs
+++ geodesic/src/main.rs
@@ -16,7 +16,6 @@ use find_folder::Search as FolderSearch;
 use glium::{DisplayBuild, Frame, IndexBuffer, Program, Surface, VertexBuffer, BackfaceCullingMode};
 use glium::index::{PrimitiveType, NoIndices};
 use rayon::prelude::*;
-use std::iter::FromIterator;
 use std::mem;
 use std::thread;
 use std::time::Duration;
@@ -245,18 +244,26 @@ impl State {
         }
     }
 
-    fn update<Events>(&mut self, events: Events, window_dimensions: (u32, u32), hidpi_factor: f32, delta_time: f32) -> Loop where
+    fn update<Events>(&mut self, events: Events) -> Loop where
         Events: IntoIterator<Item = Event>,
     {
-        self.delta_time = delta_time;
-        self.window_dimensions = window_dimensions;
-        self.hidpi_factor = hidpi_factor;
-        self.frames_per_second = 1.0 / delta_time;
-
         for event in events {
             use input::Event::*;
 
             match event {
+                TickStart { window_dimensions, hidpi_factor, delta_time } => {
+                    self.delta_time = delta_time;
+                    self.window_dimensions = window_dimensions;
+                    self.hidpi_factor = hidpi_factor;
+                    self.frames_per_second = 1.0 / delta_time;
+
+                    if self.is_dragging {
+                        self.camera_rotation_delta = Rad::new(0.0);
+                    }
+                },
+                TickEnd => {
+                    self.camera_rotation -= self.camera_rotation_delta;
+                }
                 CloseApp => return Loop::Break,
                 SetShowingStarField(value) => self.is_showing_star_field = value,
                 SetUiCapturingMouse(value) => self.is_ui_capturing_mouse = value,
@@ -271,11 +278,6 @@ impl State {
                 UpdatePlanetSubdivisions(planet_subdivs) => self.planet_subdivs = planet_subdivs,
                 NoOp => {},
             }
-        }
-
-        self.camera_rotation -= self.camera_rotation_delta;
-        if self.is_dragging {
-            self.camera_rotation_delta = Rad::new(0.0);
         }
 
         Loop::Continue
@@ -318,12 +320,12 @@ impl State {
     }
 }
 
-fn render_scene(frame: &mut Frame, state: &State, resources: &Resources, hidpi_factor: f32) {
+fn render_scene(frame: &mut Frame, state: &State, resources: &Resources) {
     let frame_dimensions = frame.get_dimensions();
 
     let mut target = RenderTarget {
         frame: frame,
-        hidpi_factor: hidpi_factor,
+        hidpi_factor: state.hidpi_factor,
         resources: resources,
         camera: state.create_scene_camera(frame_dimensions),
         hud_matrix: state.create_hud_camera(frame_dimensions),
@@ -349,10 +351,7 @@ fn render_scene(frame: &mut Frame, state: &State, resources: &Resources, hidpi_f
     // target.render_hud_text(&state.frames_per_second.to_string(), 12.0, Point2::new(2.0, 2.0), color::BLACK).unwrap();
 }
 
-fn render_ui(frame: &mut Frame,
-             ui_context: &mut UiContext,
-             ui_renderer: &mut ui::Renderer,
-             state: &State) -> Vec<Event> {
+fn render_ui(frame: &mut Frame, ui_context: &mut UiContext, ui_renderer: &mut ui::Renderer, state: &State) -> Vec<Event> {
     use input::Event::*;
 
     let ui = ui_context.frame(state.window_dimensions, state.delta_time);
@@ -427,22 +426,34 @@ fn main() {
     let mut ui_events = Vec::new();
 
     for time in times::in_seconds() {
+        use std::iter::{self, FromIterator};
+        use input::Event::*;
+
         let window = display.get_window().unwrap();
-        let window_dimensions = window.get_inner_size_points().unwrap();
-        let hidpi_factor = window.hidpi_factor();
-        let delta_time = time.delta() as f32;
-
-        // FIXME: lots of confusing mutations if the event buffer...
         let display_events = Vec::from_iter(display.poll_events());
-        ui_context.update(display_events.iter(), hidpi_factor);
-        let events = ui_events.into_iter().chain(display_events.into_iter().map(Event::from));
 
-        match state.update(events, window_dimensions, hidpi_factor, delta_time) {
+        if state.is_showing_ui {
+            ui_context.update(display_events.iter(), window.hidpi_factor());
+        }
+
+        let tick_start = TickStart {
+            window_dimensions: window.get_inner_size_points().unwrap(),
+            hidpi_factor: window.hidpi_factor(),
+            delta_time: time.delta() as f32,
+        };
+
+        let events =
+            iter::once(tick_start)
+                .chain(ui_events)
+                .chain(display_events.into_iter().map(Event::from))
+                .chain(iter::once(TickEnd));
+
+        match state.update(events) {
             Loop::Break => break,
             Loop::Continue => {
                 let mut frame = display.draw();
 
-                render_scene(&mut frame, &state, &resources, hidpi_factor);
+                render_scene(&mut frame, &state, &resources);
 
                 if state.is_showing_ui {
                     ui_events = render_ui(&mut frame, &mut ui_context, &mut ui_renderer, &state);
