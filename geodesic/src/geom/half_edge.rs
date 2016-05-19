8f79eefc53991df2debdbe130656cc2c778e46df
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2016-05-19 12:26:06 +1000

    Style cleanups for half_edge module
    - use assert_eq!
    - add missing trailing commas
    - remove extraneous clones for copyable types

--- geodesic/src/geom/half_edge.rs
+++ geodesic/src/geom/half_edge.rs
@@ -129,28 +129,28 @@ impl Mesh {
             if new_positions.contains_key(&index) {
                 continue;
             }
-            
+
             let mp = self.edge_midpoint(edge, midpoint_fn);
             let mp_index = positions.len();
             positions.push(mp);
 
-            new_positions.insert(index, mp_index.clone());
+            new_positions.insert(index, mp_index);
             // Uncomment to break stuff
             // match edge.adjacent {
             //     Some(adjacent_index) => {
-            //         new_positions.insert(adjacent_index.clone(), mp_index.clone());
-            //     }
-            //     None => ()
+            //         new_positions.insert(adjacent_index, mp_index);
+            //     },
+            //     None => {},
             // }
         }
 
         // Create our new faces
         for face in self.faces.iter() {
-            let in_e0 = face.root.clone();
-            let in_e1 = self.edges[in_e0].next.clone();
-            let in_e2 = self.edges[in_e1].next.clone();
+            let in_e0 = face.root;
+            let in_e1 = self.edges[in_e0].next;
+            let in_e2 = self.edges[in_e1].next;
 
-            assert!(self.edges[in_e2].next == in_e0);
+            assert_eq!(self.edges[in_e2].next, in_e0);
 
             // New face indices
             let f0 = faces.len();
@@ -169,55 +169,55 @@ impl Mesh {
             let e8 = e0 + 8;        let e11 = e0 + 11;
 
             // Original position indices
-            let p0 = self.edges[in_e0].position.clone();
-            let p1 = self.edges[in_e1].position.clone();
-            let p2 = self.edges[in_e2].position.clone();
+            let p0 = self.edges[in_e0].position;
+            let p1 = self.edges[in_e1].position;
+            let p2 = self.edges[in_e2].position;
 
             // Midpoint position indices
-            let p3 = new_positions.get(&in_e0).unwrap().clone();
-            let p4 = new_positions.get(&in_e1).unwrap().clone();
-            let p5 = new_positions.get(&in_e2).unwrap().clone();
+            let p3 = *new_positions.get(&in_e0).unwrap();
+            let p4 = *new_positions.get(&in_e1).unwrap();
+            let p5 = *new_positions.get(&in_e2).unwrap();
 
             faces.push(make_face(f0, e0,  e1,  e2, p0, p3, p5, &mut edges)); // face 0
             faces.push(make_face(f1, e3,  e4,  e5, p3, p1, p4, &mut edges)); // face 1
             faces.push(make_face(f2, e6,  e7,  e8, p3, p4, p5, &mut edges)); // face 2
             faces.push(make_face(f3, e9, e10, e11, p5, p4, p2, &mut edges)); // face 3
-            
 
-            split_edges.insert(in_e0.clone(), (e0.clone(), e3.clone()));
-            split_edges.insert(in_e1.clone(), (e4.clone(), e10.clone()));
-            split_edges.insert(in_e2.clone(), (e11.clone(), e2.clone()));
-            
+
+            split_edges.insert(in_e0, ( e0,  e3));
+            split_edges.insert(in_e1, ( e4, e10));
+            split_edges.insert(in_e2, (e11,  e2));
+
 
             ///////////////////////////////////////////////////////////////
             // Setup adjacency for internal edges (info we already have)
 
             // Face 0
             // edges[e0]
-            edges[e1].adjacent = Some(e6.clone());
+            edges[e1].adjacent = Some(e6);
             // edges[e2]
-            
+
             // Face 1
             // edges[e3]
             // edges[e4]
-            edges[e5].adjacent = Some(e7.clone());
-            
+            edges[e5].adjacent = Some(e7);
+
             // Face 2
-            edges[e6].adjacent = Some(e1.clone());
-            edges[e7].adjacent = Some(e5.clone());
-            edges[e8].adjacent = Some(e9.clone());
+            edges[e6].adjacent = Some(e1);
+            edges[e7].adjacent = Some(e5);
+            edges[e8].adjacent = Some(e9);
 
             // Face 3
-            edges[e9].adjacent = Some(e8.clone());
+            edges[e9].adjacent = Some(e8);
             // edges[e10]
             // edges[e11]
         }
 
-        assert!(split_edges.len() == self.edges.len());
-        
+        assert_eq!(split_edges.len(), self.edges.len());
+
         // Update adjacency for remaining edges
         for (index, &(a, b)) in split_edges.iter() {
-            let adjacent_edge = self.edges[*index].adjacent.unwrap().clone();
+            let adjacent_edge = self.edges[*index].adjacent.unwrap();
             let &(b_adjacent, a_adjacent) = split_edges.get(&adjacent_edge).unwrap();
             edges[a].adjacent = Some(a_adjacent);
             edges[b].adjacent = Some(b_adjacent);
@@ -226,7 +226,7 @@ impl Mesh {
         Mesh {
             positions: positions,
             faces: faces,
-            edges: edges
+            edges: edges,
         }
     }
 
@@ -244,12 +244,12 @@ fn make_face(f: FaceIndex, e0: EdgeIndex, e1: EdgeIndex, e2: EdgeIndex,
              edges: &mut Vec<HalfEdge>) -> Face
 {
     //println!("Making face for {} -> {} -> {}", e0, e1, e2);
-    edges.push(HalfEdge::new_boundary(p0.clone(), f.clone(), e1.clone()));
-    assert!(edges.len() == e0+1);
-    edges.push(HalfEdge::new_boundary(p1.clone(), f.clone(), e2.clone()));
-    assert!(edges.len() == e1+1);
-    edges.push(HalfEdge::new_boundary(p2.clone(), f.clone(), e0.clone()));
-    assert!(edges.len() == e2+1);
+    edges.push(HalfEdge::new_boundary(p0, f, e1));
+    assert_eq!(edges.len(), e0 + 1);
+    edges.push(HalfEdge::new_boundary(p1, f, e2));
+    assert_eq!(edges.len(), e1 + 1);
+    edges.push(HalfEdge::new_boundary(p2, f, e0));
+    assert_eq!(edges.len(), e2 + 1);
 
     Face::new(e0)
 }
