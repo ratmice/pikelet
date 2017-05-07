ef3d67faf4e55ea68b7329c90d8eb7b9a90a4b3d
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2017-05-07 20:53:55 +1000

    Clean up game logic

--- src/lib.rs
+++ src/lib.rs
@@ -74,6 +74,39 @@ impl From<InputEvent> for Event {
     }
 }
 
+#[derive(Debug, Clone)]
+struct TurntableCamera {
+    rotation: Rad<f32>,
+    rotation_delta: Rad<f32>,
+    xz_radius: f32,
+    y_height: f32,
+    near: f32,
+    far: f32,
+    zoom_factor: f32,
+    drag_factor: f32,
+}
+
+impl TurntableCamera {
+    fn compute(&self, aspect_ratio: f32) -> ComputedCamera {
+        let camera = Camera {
+            position: Point3 {
+                x: Rad::sin(self.rotation) * self.xz_radius,
+                y: self.y_height,
+                z: Rad::cos(self.rotation) * self.xz_radius,
+            },
+            target: Point3::origin(),
+            projection: PerspectiveFov {
+                aspect: aspect_ratio,
+                fovy: Rad::full_turn() / 6.0,
+                near: self.near,
+                far: self.far,
+            },
+        };
+
+        camera.compute()
+    }
+}
+
 #[derive(Clone, Debug)]
 struct StarField {
     count: usize,
@@ -95,14 +128,7 @@ struct State {
 
     light_dir: Vector3<f32>,
 
-    camera_rotation: Rad<f32>,
-    camera_rotation_delta: Rad<f32>,
-    camera_xz_radius: f32,
-    camera_y_height: f32,
-    camera_near: f32,
-    camera_far: f32,
-    camera_zoom_factor: f32,
-    camera_drag_factor: f32,
+    camera: TurntableCamera,
 
     star_field_radius: f32,
     stars0: StarField,
@@ -129,14 +155,16 @@ impl State {
             frame_metrics: frame_metrics,
             mouse_position: Point2::origin(),
 
-            camera_rotation: Rad(0.0),
-            camera_rotation_delta: Rad(0.0),
-            camera_xz_radius: 2.0,
-            camera_y_height: 1.0,
-            camera_near: 0.1,
-            camera_far: 1000.0,
-            camera_zoom_factor: 10.0,
-            camera_drag_factor: 10.0,
+            camera: TurntableCamera {
+                rotation: Rad(0.0),
+                rotation_delta: Rad(0.0),
+                xz_radius: 2.0,
+                y_height: 1.0,
+                near: 0.1,
+                far: 1000.0,
+                zoom_factor: 10.0,
+                drag_factor: 10.0,
+            },
 
             star_field_radius: 10.0,
             stars0: StarField {
@@ -161,30 +189,8 @@ impl State {
         *self = State::new(self.frame_metrics);
     }
 
-    fn scene_camera_position(&self) -> Point3<f32> {
-        Point3 {
-            x: Rad::sin(self.camera_rotation) * self.camera_xz_radius,
-            y: self.camera_y_height,
-            z: Rad::cos(self.camera_rotation) * self.camera_xz_radius,
-        }
-    }
-
-    fn scene_camera_projection(&self) -> PerspectiveFov<f32> {
-        PerspectiveFov {
-            aspect: self.frame_metrics.aspect_ratio(),
-            fovy: Rad::full_turn() / 6.0,
-            near: self.camera_near,
-            far: self.camera_far,
-        }
-    }
-
     fn create_scene_camera(&self) -> ComputedCamera {
-        Camera {
-                position: self.scene_camera_position(),
-                target: Point3::origin(),
-                projection: self.scene_camera_projection(),
-            }
-            .compute()
+        self.camera.compute(self.frame_metrics.aspect_ratio())
     }
 
     fn create_hud_camera(&self) -> Matrix4<f32> {
@@ -196,123 +202,7 @@ impl State {
 pub struct Game {
     state: State,
     job_tx: Sender<Job>,
-}
-
-fn init_resources(resources: ResourcesRef) {
-    use std::fs::File;
-    use std::io;
-    use std::io::prelude::*;
-    use std::path::Path;
-
-    let assets = FolderSearch::ParentsThenKids(3, 3)
-        .for_folder("resources")
-        .expect("Could not locate `resources` folder");
-
-    fn load_shader(path: &Path) -> io::Result<String> {
-        let mut file = File::open(path)?;
-        let mut buffer = String::new();
-        file.read_to_string(&mut buffer)?;
-
-        Ok(buffer)
-    }
-
-    resources
-        .compile_program("flat_shaded",
-                         load_shader(&assets.join("shaders/flat_shaded.v.glsl")).unwrap(),
-                         load_shader(&assets.join("shaders/flat_shaded.f.glsl")).unwrap())
-        .unwrap();
-
-    resources
-        .compile_program("text",
-                         load_shader(&assets.join("shaders/text.v.glsl")).unwrap(),
-                         load_shader(&assets.join("shaders/text.f.glsl")).unwrap())
-        .unwrap();
-
-    resources
-        .compile_program("unshaded",
-                         load_shader(&assets.join("shaders/unshaded.v.glsl")).unwrap(),
-                         load_shader(&assets.join("shaders/unshaded.f.glsl")).unwrap())
-        .unwrap();
-
-    fn load_font(path: &Path) -> io::Result<Vec<u8>> {
-        let mut file = File::open(path)?;
-        let mut buffer = vec![];
-        file.read_to_end(&mut buffer)?;
-
-        Ok(buffer)
-    }
-
-    resources
-        .upload_font("blogger_sans",
-                     load_font(&assets.join("fonts/blogger_sans.ttf")).unwrap())
-        .unwrap();
-}
-
-impl Application for Game {
-    type Event = Event;
-
-    fn init(frame_metrics: FrameMetrics, resources: ResourcesRef) -> Game {
-        let game = Game {
-            state: State::new(frame_metrics),
-            job_tx: {
-                let resources = resources.clone();
-                job_queue::spawn(move |job| Job::process(job, &resources))
-            },
-        };
-
-        init_resources(resources);
-        game.queue_regenete_stars_jobs();
-        game.queue_regenete_planet_job();
-
-        game
-    }
-
-    fn handle_frame_request(&mut self, frame_metrics: FrameMetrics) -> Loop {
-        self.state.frame_metrics = frame_metrics;
-        self.state.camera_rotation -= self.state.camera_rotation_delta;
-
-        if self.state.is_dragging {
-            self.state.camera_rotation_delta = Rad(0.0);
-        }
-
-        Loop::Continue
-    }
-
-    fn handle_input(&mut self, event: Event) -> Loop {
-        use self::Event::*;
-
-        match event {
-            Close => return Loop::Break,
-            SetLimitingFps(value) => self.state.is_limiting_fps = value,
-            SetPlanetRadius(value) => self.state.planet_radius = value,
-            SetPlanetSubdivisions(planet_subdivs) => {
-                self.state.planet_subdivs = planet_subdivs;
-                self.queue_regenete_planet_job();
-            },
-            SetShowingStarField(value) => self.state.is_showing_star_field = value,
-            SetStarFieldRadius(value) => self.state.star_field_radius = value,
-            SetUiCapturingMouse(value) => self.state.is_ui_capturing_mouse = value,
-            SetWireframe(value) => self.state.is_wireframe = value,
-            ToggleUi => self.state.is_ui_enabled = !self.state.is_ui_enabled,
-            ResetState => self.state.reset(),
-            DragStart => self.state.is_dragging = !self.state.is_ui_capturing_mouse,
-            DragEnd => self.state.is_dragging = false,
-            ZoomStart => self.state.is_zooming = !self.state.is_ui_capturing_mouse,
-            ZoomEnd => self.state.is_zooming = false,
-            MousePosition(position) => self.handle_mouse_update(position),
-            NoOp => {},
-        }
-
-        Loop::Continue
-    }
-
-    fn render(&self) -> RenderData<Event> {
-        RenderData {
-            metrics: self.state.frame_metrics,
-            is_limiting_fps: self.state.is_limiting_fps,
-            command_list: self.create_command_list(),
-        }
-    }
+    resources: ResourcesRef,
 }
 
 impl Game {
@@ -339,29 +229,133 @@ impl Game {
                        });
     }
 
-    fn handle_mouse_update(&mut self, new_position: Point2<i32>) {
-        use std::mem;
+    fn init_resources(&self) {
+        use std::fs::File;
+        use std::io;
+        use std::io::prelude::*;
+        use std::path::Path;
 
-        let old_position = mem::replace(&mut self.state.mouse_position, new_position);
-        let mouse_delta = new_position - old_position;
+        let assets = FolderSearch::ParentsThenKids(3, 3)
+            .for_folder("resources")
+            .expect("Could not locate `resources` folder");
 
-        if !self.state.is_ui_capturing_mouse {
-            if self.state.is_dragging {
-                let size_points = self.state.frame_metrics.size_points;
-                let rotations_per_second = (mouse_delta.x as f32 / size_points.width as f32) *
-                                           self.state.camera_drag_factor;
-                self.state.camera_rotation_delta = Rad::full_turn() * rotations_per_second *
-                                                   self.state.frame_metrics.delta_time;
-            }
+        fn load_shader(path: &Path) -> io::Result<String> {
+            let mut file = File::open(path)?;
+            let mut buffer = String::new();
+            file.read_to_string(&mut buffer)?;
 
-            if self.state.is_zooming {
-                let zoom_delta = mouse_delta.x as f32 * self.state.frame_metrics.delta_time;
-                self.state.camera_xz_radius -= zoom_delta * self.state.camera_zoom_factor;
-            }
+            Ok(buffer)
         }
+
+        self.resources
+            .compile_program("flat_shaded",
+                             load_shader(&assets.join("shaders/flat_shaded.v.glsl")).unwrap(),
+                             load_shader(&assets.join("shaders/flat_shaded.f.glsl")).unwrap())
+            .unwrap();
+
+        self.resources
+            .compile_program("text",
+                             load_shader(&assets.join("shaders/text.v.glsl")).unwrap(),
+                             load_shader(&assets.join("shaders/text.f.glsl")).unwrap())
+            .unwrap();
+
+        self.resources
+            .compile_program("unshaded",
+                             load_shader(&assets.join("shaders/unshaded.v.glsl")).unwrap(),
+                             load_shader(&assets.join("shaders/unshaded.f.glsl")).unwrap())
+            .unwrap();
+
+        fn load_font(path: &Path) -> io::Result<Vec<u8>> {
+            let mut file = File::open(path)?;
+            let mut buffer = vec![];
+            file.read_to_end(&mut buffer)?;
+
+            Ok(buffer)
+        }
+
+        self.resources
+            .upload_font("blogger_sans",
+                         load_font(&assets.join("fonts/blogger_sans.ttf")).unwrap())
+            .unwrap();
+    }
+}
+
+impl Application for Game {
+    type Event = Event;
+
+    fn init(frame_metrics: FrameMetrics, resources: ResourcesRef) -> Game {
+        let game = Game {
+            state: State::new(frame_metrics),
+            resources: resources.clone(),
+            job_tx: job_queue::spawn(move |job| Job::process(job, &resources)),
+        };
+
+        game.init_resources();
+        game.queue_regenete_stars_jobs();
+        game.queue_regenete_planet_job();
+
+        game
     }
 
-    fn create_command_list(&self) -> CommandList<Event> {
+    fn handle_frame_request(&mut self, frame_metrics: FrameMetrics) -> Loop {
+        self.state.frame_metrics = frame_metrics;
+        self.state.camera.rotation -= self.state.camera.rotation_delta;
+
+        if self.state.is_dragging {
+            self.state.camera.rotation_delta = Rad(0.0);
+        }
+
+        Loop::Continue
+    }
+
+    fn handle_input(&mut self, event: Event) -> Loop {
+        use std::mem;
+        use self::Event::*;
+
+        match event {
+            Close => return Loop::Break,
+            SetLimitingFps(value) => self.state.is_limiting_fps = value,
+            SetPlanetRadius(value) => self.state.planet_radius = value,
+            SetPlanetSubdivisions(planet_subdivs) => {
+                self.state.planet_subdivs = planet_subdivs;
+                self.queue_regenete_planet_job();
+            },
+            SetShowingStarField(value) => self.state.is_showing_star_field = value,
+            SetStarFieldRadius(value) => self.state.star_field_radius = value,
+            SetUiCapturingMouse(value) => self.state.is_ui_capturing_mouse = value,
+            SetWireframe(value) => self.state.is_wireframe = value,
+            ToggleUi => self.state.is_ui_enabled = !self.state.is_ui_enabled,
+            ResetState => self.state.reset(),
+            DragStart => self.state.is_dragging = !self.state.is_ui_capturing_mouse,
+            DragEnd => self.state.is_dragging = false,
+            ZoomStart => self.state.is_zooming = !self.state.is_ui_capturing_mouse,
+            ZoomEnd => self.state.is_zooming = false,
+            MousePosition(new_position) => {
+                let old_position = mem::replace(&mut self.state.mouse_position, new_position);
+                let mouse_delta = new_position - old_position;
+
+                if !self.state.is_ui_capturing_mouse {
+                    if self.state.is_dragging {
+                        let size_points = self.state.frame_metrics.size_points;
+                        let rps = (mouse_delta.x as f32 / size_points.width as f32) *
+                                  self.state.camera.drag_factor;
+                        self.state.camera.rotation_delta = Rad::full_turn() * rps *
+                                                           self.state.frame_metrics.delta_time;
+                    }
+
+                    if self.state.is_zooming {
+                        let zoom_delta = mouse_delta.y as f32 * self.state.frame_metrics.delta_time;
+                        self.state.camera.xz_radius -= zoom_delta * self.state.camera.zoom_factor;
+                    }
+                }
+            },
+            NoOp => {},
+        }
+
+        Loop::Continue
+    }
+
+    fn render(&self) -> RenderData<Event> {
         let mut command_list = CommandList::new();
 
         let camera = self.state.create_scene_camera();
@@ -433,6 +427,10 @@ impl Game {
             command_list.ui(move |ui| debug_controls.render(ui));
         }
 
-        command_list
+        RenderData {
+            metrics: self.state.frame_metrics,
+            is_limiting_fps: self.state.is_limiting_fps,
+            command_list,
+        }
     }
 }
