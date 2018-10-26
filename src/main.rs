cccf68f27803c558aaf5cf7f567844b13ba9de84
Chip Collier <photex@gmail.com>
2018-10-26 14:31:06 +0200

    Input established
    - Window can now be closed by Esc
    - Window captures input but nothing to see yet

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,76 @@
 extern crate amethyst;
 
 use amethyst::{
+    controls::{FlyControlBundle, FlyControlTag},
+    core::transform::{GlobalTransform, Transform, TransformBundle},
+    ecs::{Read, System, Write},
+    input::{InputBundle, is_close_requested, is_key_down},
     prelude::*,
-    renderer::{DisplayConfig, DrawFlat, Pipeline, PosNormTex, RenderBundle, Stage},
+    renderer::*,
     utils::application_root_dir,
 };
 
 struct Example;
 
-impl<'a, 'b> SimpleState<'a, 'b> for Example {}
+impl<'a, 'b> SimpleState<'a, 'b> for Example {
+    fn handle_event(
+        &mut self,
+        data: StateData<GameData>,
+        event: StateEvent,
+    ) -> SimpleTrans<'a, 'b> {
+        let StateData { world, .. } = data;
+        if let StateEvent::Window(event) = &event {
+            if is_close_requested(&event) || is_key_down(&event, VirtualKeyCode::Escape) {
+                Trans::Quit
+            } else {
+                Trans::None
+            }
+        } else {
+            Trans::None
+        }
+    }
+}
 
 fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
 
-    let path = format!(
-        "{}/resources/display_config.ron",
-        application_root_dir()
-    );
-    let config = DisplayConfig::load(&path);
+    let app_root = application_root_dir();
 
-    let pipe = Pipeline::build().with_stage(
-        Stage::with_backbuffer()
-            .clear_target([0.00196, 0.23726, 0.21765, 1.0], 1.0)
-            .with_pass(DrawFlat::<PosNormTex>::new()),
-    );
+    let input_bundle = {
+        let path = format!("{}/resources/input.ron", app_root);
+        InputBundle::<String, String>::new().with_bindings_from_file(path)?
+    };
 
-    let game_data =
-        GameDataBuilder::default().with_bundle(RenderBundle::new(pipe, Some(config)))?;
+    let fly_control_bundle = FlyControlBundle::<String, String>::new(
+        Some(String::from("move_x")),
+        Some(String::from("move_y")),
+        Some(String::from("move_z")),
+    ).with_sensitivity(0.1, 0.1);
+
+    let transform_bundle = TransformBundle::new().with_dep(&["fly_movement"]);
+
+    let render_bundle = {
+        let display_config = {
+            let path = format!("{}/resources/display_config.ron", app_root);
+            DisplayConfig::load(&path)
+        };
+
+        let pipe = Pipeline::build().with_stage(
+            Stage::with_backbuffer()
+                .clear_target([0.00196, 0.23726, 0.21765, 1.0], 1.0)
+                .with_pass(DrawFlat::<PosNormTex>::new()),
+        );
+
+        RenderBundle::new(pipe, Some(display_config))
+    };
+
+    let game_data = GameDataBuilder::default()
+        .with_bundle(input_bundle)?
+        .with_bundle(fly_control_bundle)?
+        .with_bundle(transform_bundle)?
+        .with_bundle(render_bundle)?;
+
     let mut game = Application::new("./", Example, game_data)?;
-
     game.run();
 
     Ok(())
