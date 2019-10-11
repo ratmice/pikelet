b8063ad9a93113386c6ddfccadc955893cf0aa38
Chip Collier <photex@gmail.com>
2019-10-11 10:13:19 +0200

    Figuring out how to structure this a bit.
    Using vange-rs as a guide. Considering starting a gitbook about wgpu-rs

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,19 @@
 mod system;
 
 use std::env;
+use std::io::{Error as IoError};
+use std::cell::RefCell;
+
 use log;
 use env_logger;
+
 use winit::event;
 use winit::event_loop::EventLoop;
+use winit::window::Window;
+use winit::error::OsError;
+
 use system::rendering;
+
 
 #[derive(Clone, Debug)]
 #[allow(unused)]
@@ -18,9 +26,94 @@ pub enum VoyagerError {
     Config(String),
 }
 
+impl From<OsError> for VoyagerError {
+    fn from(err: OsError) -> Self {
+        VoyagerError::Runtime(err.to_string())
+    }
+}
+
+impl From<IoError> for VoyagerError {
+    fn from(err: IoError) -> Self {
+        VoyagerError::Runtime(format!("IO Error: {}", err.to_string()))
+    }
+}
+
 pub type VoyagerResult<T> = Result<T, VoyagerError>;
 
 const VOYAGER_LOG: &str = "VOYAGER_LOG";
+
+
+struct AppContext {
+    event_loop: EventLoop<()>,
+    window: Window,
+    surface: wgpu::Surface,
+    adapter: wgpu::Adapter,
+    device: wgpu::Device,
+    queue: RefCell<wgpu::Queue>,
+    swap_chain: RefCell<wgpu::SwapChain>,
+}
+
+impl AppContext {
+    pub fn initialize() -> VoyagerResult<Self> {
+        log::trace!("Creating window system event loop");
+        let event_loop = EventLoop::new();
+        log::trace!("Creating window");
+        let window = Window::new(&event_loop)?;
+        log::trace!("Creating wgpu surface");
+        let surface = wgpu::Surface::create(&window);
+        log::trace!("Requesting wgpu adapter");;
+        let adapter = {
+            let req = wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::Default,
+                backends: wgpu::BackendBit::PRIMARY,
+            };
+            match wgpu::Adapter::request(&req) {
+                Some(adapter) => adapter,
+                None => {
+                    return Err(VoyagerError::Runtime("No available adapter.".to_string()));
+                },
+            }
+        };
+        log::trace!("Requesting wgpu device and queue");
+        let (device, mut queue) = {
+            let desc = wgpu::DeviceDescriptor {
+                extensions: wgpu::Extensions {
+                    anisotropic_filtering: false,
+                },
+                limits: wgpu::Limits::default(),
+            };
+            adapter.request_device(&desc)
+        };
+        log::trace!("Creating swap chain");
+        let size = window.inner_size().to_physical(window.hidpi_factor());
+        let mut swap_chain = device.create_swap_chain(
+            &surface,
+            &wgpu::SwapChainDescriptor {
+                usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+                format: wgpu::TextureFormat::Bgra8UnormSrgb,
+                width: size.width.round() as u32,
+                height: size.height.round() as u32,
+                present_mode: wgpu::PresentMode::Vsync,
+            }
+        );
+
+        Ok(AppContext {
+            event_loop,
+            window,
+            surface,
+            adapter,
+            device,
+            queue: RefCell::new(queue),
+            swap_chain: RefCell::new(swap_chain),
+        })
+    }
+
+    pub fn window_size(&self) -> (u32, u32) {
+        self.window.inner_size()
+            .to_physical(self.window.hidpi_factor())
+            .into()
+    }
+}
 
 
 fn main() -> VoyagerResult<()> {
@@ -30,11 +123,10 @@ fn main() -> VoyagerResult<()> {
     env_logger::init_from_env(VOYAGER_LOG);
     log::info!("Voyager startup.");
 
-    log::trace!("Creating window system event loop");
-    let event_loop = EventLoop::new();
+    let mut app = AppContext::initialize()?;
 
     log::debug!("Initializing renderer.");
-    let renderer = rendering::Renderer::initialize(&event_loop)?;
+    let renderer = rendering::Renderer::initialize(&mut app.device)?;
 
     log::info!("Voyager shutdown.");
     Ok(())
--- src/system/rendering.rs
+++ src/system/rendering.rs
@@ -3,82 +3,118 @@ use crate::{VoyagerResult, VoyagerError};
 
 use log;
 use std::cell::RefCell;
-//use winit::event;
-use winit::event_loop::EventLoop;
-use winit::window::Window;
-use winit::error::OsError;
-use wgpu::{Surface, Adapter, Device, Queue, SwapChain};
 
-impl From<OsError> for VoyagerError {
-    fn from(err: OsError) -> Self {
-        VoyagerError::Runtime(err.to_string())
-    }
+
+pub struct Pass<'enc> {
+    frame: wgpu::SwapChainOutput<'enc>,
+    pass: wgpu::RenderPass<'enc>,
 }
 
-
 pub struct Renderer {
-    window: Window,
-    surface: Surface,
-    adapter: Adapter,
-    device: Device,
-    queue: RefCell<Queue>,
-    swap_chain: RefCell<SwapChain>,
+    bind_group: wgpu::BindGroup,
+    pipeline: wgpu::RenderPipeline,
+    encoder: RefCell<wgpu::CommandEncoder>,
 }
 
 impl Renderer {
-    pub fn initialize(event_loop: &EventLoop<()>) -> VoyagerResult<Renderer> {
-        log::trace!("Creating window");
-        let window = Window::new(&event_loop)?;
-        log::trace!("Creating wgpu surface");
-        let surface = wgpu::Surface::create(&window);
-        log::trace!("Requesting wgpu adapter");;
-        let adapter = {
-            let req = wgpu::RequestAdapterOptions {
-                power_preference: wgpu::PowerPreference::Default,
-                backends: wgpu::BackendBit::PRIMARY,
-            };
-            match Adapter::request(&req) {
-                Some(adapter) => adapter,
-                None => {
-                    return Err(VoyagerError::Runtime("No available adapter.".to_string()));
-                },
-            }
-        };
-        log::trace!("Requesting wgpu device and queue");
-        let (device, mut queue) = {
-            let desc = wgpu::DeviceDescriptor {
-                extensions: wgpu::Extensions {
-                    anisotropic_filtering: false,
-                },
-                limits: wgpu::Limits::default(),
-            };
-            adapter.request_device(&desc)
-        };
-        log::trace!("Creating swap chain");
-        let size = window.inner_size().to_physical(window.hidpi_factor());
-        let mut swap_chain = device.create_swap_chain(
-            &surface,
-            &wgpu::SwapChainDescriptor {
-                usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
-                format: wgpu::TextureFormat::Bgra8UnormSrgb,
-                width: size.width.round() as u32,
-                height: size.height.round() as u32,
-                present_mode: wgpu::PresentMode::Vsync,
-            }
+    pub fn initialize(device: &mut wgpu::Device) -> VoyagerResult<Renderer> {
+        let vs = include_bytes!("../../data/triangle.vert.spv");
+        let fs = include_bytes!("../../data/triangle.frag.spv");
+
+        log::trace!("Creating vertex shader module for default pipeline");
+        let vs_module = device.create_shader_module(
+            &wgpu::read_spirv(
+                std::io::Cursor::new(&vs[..])
+            )?
         );
+
+        log::trace!("Creating fragment shader module for default pipeline");
+        let fs_module = device.create_shader_module(
+            &wgpu::read_spirv(
+                std::io::Cursor::new(&fs[..])
+            )?
+        );
+
+        let bind_group_layout = device.create_bind_group_layout(
+            &wgpu::BindGroupLayoutDescriptor {
+                bindings: &[],
+            });
+        let bind_group = device.create_bind_group(
+            &wgpu::BindGroupDescriptor {
+                layout: &bind_group_layout,
+                bindings: &[],
+            });
+        let pipeline_layout = device.create_pipeline_layout(
+            &wgpu::PipelineLayoutDescriptor {
+                bind_group_layouts: &[&bind_group_layout],
+            });
+
+        let pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            layout: &pipeline_layout,
+            vertex_stage: wgpu::ProgrammableStageDescriptor {
+                module: &vs_module,
+                entry_point: "main",
+            },
+            fragment_stage: Some(wgpu::ProgrammableStageDescriptor {
+                module: &fs_module,
+                entry_point: "main",
+            }),
+            rasterization_state: Some(wgpu::RasterizationStateDescriptor {
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: wgpu::CullMode::None,
+                depth_bias: 0,
+                depth_bias_slope_scale: 0.0,
+                depth_bias_clamp: 0.0,
+            }),
+            primitive_topology: wgpu::PrimitiveTopology::TriangleList,
+            color_states: &[wgpu::ColorStateDescriptor {
+                format: wgpu::TextureFormat::Bgra8UnormSrgb,
+                color_blend: wgpu::BlendDescriptor::REPLACE,
+                alpha_blend: wgpu::BlendDescriptor::REPLACE,
+                write_mask: wgpu::ColorWrite::ALL,
+            }],
+            depth_stencil_state: None,
+            index_format: wgpu::IndexFormat::Uint16,
+            vertex_buffers: &[],
+            sample_count: 1,
+            sample_mask: !0,
+            alpha_to_coverage_enabled: false,
+        });
+
+        let encoder = device.create_command_encoder(
+            &wgpu::CommandEncoderDescriptor {
+                todo: 0,
+            });
+
         Ok(Renderer {
-            window,
-            surface,
-            adapter,
-            device,
-            queue: RefCell::new(queue),
-            swap_chain: RefCell::new(swap_chain),
+            bind_group,
+            pipeline,
+            encoder: RefCell::new(encoder),
         })
     }
 
-    pub fn window_size(&self) -> (u32, u32) {
-        self.window.inner_size()
-            .to_physical(self.window.hidpi_factor())
-            .into()
+    pub fn begin_pass<'a>(
+        &self,
+        encoder: &'a mut wgpu::CommandEncoder,
+        swap_chain: &'a mut wgpu::SwapChain,
+    ) -> Pass<'a> {
+        let frame = swap_chain.get_next_texture();
+
+        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
+                attachment: &frame.view,
+                resolve_target: None,
+                load_op: wgpu::LoadOp::Clear,
+                store_op: wgpu::StoreOp::Store,
+                clear_color: wgpu::Color::BLUE,
+            }],
+            depth_stencil_attachment: None,
+        });
+        render_pass.set_pipeline(&self.pipeline);
+        render_pass.set_bind_group(0, &self.bind_group, &[]);
+        Pass {
+            frame,
+            pass: render_pass,
+        }
     }
 }
